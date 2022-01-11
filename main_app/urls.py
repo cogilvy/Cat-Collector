cc9f636eea4f8f1073410cdc5aa23731933a7359
cogilvy <chrisogilvy@cox.net>
2022-01-11 12:02:01 -0800

    Final Code - CBV

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,15 @@ urlpatterns = [
   # 'cats/' - Cats Index Route
   path('cats/', views.cats_index, name='cats_index'),
   
-  # 'cats/<int:cat_id>/' - Cat Details Route
+  # 'cats/<int:cat_id>/' - Cats Details Route
   path('cats/<int:cat_id>/', views.cats_detail, name='cats_detail'),
+
+  # 'cats/create/' - Cats Create Route
+  path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
+
+  # 'cats/<int:pk>/update/ - Cats Update Route
+  path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
+
+  # 'cats/<int:pk>/delete/ - Cats Delete Route
+  path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
 ]
