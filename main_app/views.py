ed1c7944d1a48087652db0977efff80863e3009a
cogilvy <chrisogilvy@cox.net>
2022-01-12 15:03:42 -0800

    Final Code - Many:Many

--- main_app/views.py
+++ main_app/views.py
@@ -20,14 +20,17 @@ def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
   # instantiate FeedingForm to be rendered in the detail.html template
   feeding_form = FeedingForm()
+  # find all toys not associated with this cat
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in=cat.toys.all().values_list('id'))
   return render(request, 'cats/detail.html', {
     'cat': cat,
-    'feeding_form': feeding_form
+    'feeding_form': feeding_form,
+    'toys': toys_cat_doesnt_have
   })
 
 class CatCreate(CreateView):
   model = Cat
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age']
 
 class CatUpdate(UpdateView):
   model = Cat
@@ -64,3 +67,8 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
   model = Toy
   success_url = '/toys/'
+
+def assoc_toy(request, cat_id, toy_id):
+  cat = Cat.objects.get(id=cat_id)
+  cat.toys.add(toy_id)
+  return redirect('cats_detail', cat_id=cat_id)
--- main_app1/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Cat)
-admin.site.register(Feeding)
